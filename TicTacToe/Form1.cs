dcc9f22c0d849e90893e6b5ab586e607d8fc1309
Haydar Yilmaz <Haydar.yilmaz@gmail.com>
2014-04-20 16:19:29 +0200

    added  removeText() && checkForWinner draw is still bugged

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,8 +12,8 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
-        private bool turn = true;
-        private int turn_count = 0;
+        private int _turn = 0;
+        private string image = "";
         public Form1()
         {
             InitializeComponent();
@@ -32,20 +32,55 @@ namespace TicTacToe
         private void button_click(object sender, EventArgs e)
         {
             Button b = (Button) sender;
-            if (turn)
-                b.Text = "X";
+            if (_turn%2 == 1)
+                image = "X";
             else
-                b.Text = "O";
-            turn = !turn;
-            b.Enabled = false;
+                image = "O";
+            if (b.Text.Equals(""))
+                b.Text = image;
+            _turn++;
+
+            if (!(checkForWinner(button1, button2, button3) ||
+            checkForWinner(button4, button5, button6) ||
+            checkForWinner(button7, button8, button9) ||
+            checkForWinner(button1, button5, button9) ||
+            checkForWinner(button3, button5, button7) ||
+            checkForWinner(button1, button4, button7) ||
+            checkForWinner(button2, button5, button8) ||
+            checkForWinner(button3, button6, button9)) && _turn == 9)
+            {
+                System.Windows.Forms.MessageBox.Show("Draw!.",
+                    "there is no winner!");
+                removeText();
+            }
+
         }
 
-        private void checkForWinner()
-        {
-            bool there_is_a_winner = false;
 
-          // The buttons, dont have identifiers yet.  
-            //yes
+        private void removeText()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+
+
+
+
+        private bool checkForWinner(Button b1, Button b2, Button b3) 
+        {
+           if (b1.Text.Equals(b2.Text) &&  b1.Text.Equals(b3.Text) && !b1.Text.Equals(""))
+            {
+                MessageBox.Show("Congratulations player " + image + " won", "there is a winner!"); removeText();
+                return true;
+            }
+            return false;
         }
 
 
