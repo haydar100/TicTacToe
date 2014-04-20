ad4a6dea10fd576ac43630952e85a725d19962ef
Haydar Yilmaz <Haydar.yilmaz@gmail.com>
2014-04-20 16:45:48 +0200

    Analyzed with resharper

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TicTacToe
 {
     public partial class Form1 : Form
     {
-        private int _turn = 0;
-        private string image = "";
+        private int _turn;
+        private string _image = "";
         public Form1()
         {
             InitializeComponent();
@@ -31,33 +24,27 @@ namespace TicTacToe
 
         private void button_click(object sender, EventArgs e)
         {
-            Button b = (Button) sender;
-            if (_turn%2 == 1)
-                image = "X";
-            else
-                image = "O";
+            var b = (Button) sender;
+            _image = _turn%2 == 1 ? "X" : "O";
             if (b.Text.Equals(""))
-                b.Text = image;
+                b.Text = _image;
             _turn++;
 
-            if (!(checkForWinner(button1, button2, button3) ||
-            checkForWinner(button4, button5, button6) ||
-            checkForWinner(button7, button8, button9) ||
-            checkForWinner(button1, button5, button9) ||
-            checkForWinner(button3, button5, button7) ||
-            checkForWinner(button1, button4, button7) ||
-            checkForWinner(button2, button5, button8) ||
-            checkForWinner(button3, button6, button9)) && _turn == 9)
-            {
-                System.Windows.Forms.MessageBox.Show("Draw!.",
-                    "there is no winner!");
-                removeText();
-            }
-
+            if (CheckForWinner(button1, button2, button3) ||
+                CheckForWinner(button4, button5, button6) ||
+                CheckForWinner(button7, button8, button9) ||
+                CheckForWinner(button1, button5, button9) ||
+                CheckForWinner(button3, button5, button7) ||
+                CheckForWinner(button1, button4, button7) ||
+                CheckForWinner(button2, button5, button8) ||
+                CheckForWinner(button3, button6, button9) || _turn != 9) return;
+            MessageBox.Show("Draw!.",
+                "there is no winner!");
+            RemoveText();
         }
 
 
-        private void removeText()
+        private void RemoveText()
         {
             button1.Text = "";
             button2.Text = "";
@@ -73,11 +60,11 @@ namespace TicTacToe
 
 
 
-        private bool checkForWinner(Button b1, Button b2, Button b3) 
+        private bool CheckForWinner(Button b1, Button b2, Button b3) 
         {
            if (b1.Text.Equals(b2.Text) &&  b1.Text.Equals(b3.Text) && !b1.Text.Equals(""))
             {
-                MessageBox.Show("Congratulations player " + image + " won", "there is a winner!"); removeText();
+                MessageBox.Show("Congratulations player " + _image + " won", "there is a winner!"); RemoveText();
                 return true;
             }
             return false;
--- TicTacToe/Properties/Resources.Designer.cs
+++ TicTacToe/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TicTacToe.Properties
-{
-
-
+namespace TicTacToe.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,59 @@ namespace TicTacToe.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TicTacToe.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Haydar IV2A trolol.
+        /// </summary>
+        internal static string Form1_aboutToolStripMenuItem_Click_Haydar_IV2A_trolol {
+            get {
+                return ResourceManager.GetString("Form1_aboutToolStripMenuItem_Click_Haydar_IV2A_trolol", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Draw!..
+        /// </summary>
+        internal static string Form1_button_click_Draw__ {
+            get {
+                return ResourceManager.GetString("Form1_button_click_Draw__", resourceCulture);
             }
         }
     }
